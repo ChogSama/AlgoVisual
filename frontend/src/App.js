242c7973f8ec6447de8a6303cfe5c05bd3036816
ChogSama <chogsama123@gmail.com>
2025-11-23 12:39:15 +0700

    Convert frontend folder from submodule to normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+import BubbleSortVisualizer from './components/BubbleSortVisualizer.js';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="app-title">AlgoVisual</h1>
+      <BubbleSortVisualizer />
+    </div>
+  );
+}
+
+export default App;
